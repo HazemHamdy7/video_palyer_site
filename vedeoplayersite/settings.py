56111ce92f2a2ea39dfa6b314d8dc7ea35940562
HazemHamdy7 <HazemHamdy7.com>
2023-12-15 20:16:11 +0200

    add database in project postgres

--- vedeoplayersite/settings.py
+++ vedeoplayersite/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'vedeoplayersite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'database',
+        'USER': 'fl0user',
+        'PASSWORD': 'YlM91kvNbBEw',
+        'HOST': 'ep-plain-heart-64053385.ap-southeast-1.aws.neon.fl0.io',
+        'PORT': '5432',
     }
 }
 
